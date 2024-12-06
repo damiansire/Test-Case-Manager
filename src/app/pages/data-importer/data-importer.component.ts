f9641c6ac77eb3a662c4515e9a83e2270c2851da
Damian Sire <damiansire@gmail.com>
2024-12-06 04:09:51 -0300

    structure: add data importer section

---
+++ src/app/pages/data-importer/data-importer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-data-importer',
+  imports: [],
+  templateUrl: './data-importer.component.html',
+  styleUrl: './data-importer.component.css'
+})
+export class DataImporterComponent {
+
+}
