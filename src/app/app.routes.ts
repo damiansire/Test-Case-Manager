a44c4892ca99c46d3ee0f2e6a55adf3f6ef86d10
Damian Sire <damiansire@gmail.com>
2024-12-05 23:42:01 -0300

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { TestsVerificationComponent } from './pages/tests-verification/tests-verification.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: TestsVerificationComponent
+    }
+];
