7fb7eecad42b20331c9c3b9b5f9159e6561ed9ca
Damian Sire <damiansire@gmail.com>
2024-12-06 04:33:41 -0300

    feat: add basic logic for upload file

--- src/app/pages/data-importer/data-importer.component.ts
+++ src/app/pages/data-importer/data-importer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CsvHandlerService } from '../../services/csv-handler';
 
 @Component({
   selector: 'app-data-importer',
@@ -7,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrl: './data-importer.component.css'
 })
 export class DataImporterComponent {
+  selectedFile: File | null = null;
 
+  csvHandlerService = inject(CsvHandlerService);
+  
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      this.selectedFile = input.files[0];
+    }
+  }
+  
+  onUpload(): void {
+    if (this.selectedFile) {
+      const reader = new FileReader();
+  
+      reader.onload = (e: any) => {
+        const csvData: string = e.target.result;
+
+        const lines = csvData.split('\n');
+        const headers = lines[0].split(',');
+
+      };
+  
+      reader.readAsText(this.selectedFile);
+    } else {
+      console.log('No file selected');
+    }
+  }
 }
