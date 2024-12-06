99f2a5f8887baf5e510e49026b76fbdd2cfc19c4
Damian Sire <damiansire@gmail.com>
2024-12-05 23:42:21 -0300

    add components and models for test verification

---
+++ src/app/pages/tests-verification/tests-verification.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tests-verification',
+  imports: [],
+  templateUrl: './tests-verification.component.html',
+  styleUrl: './tests-verification.component.css'
+})
+export class TestsVerificationComponent {
+
+}
